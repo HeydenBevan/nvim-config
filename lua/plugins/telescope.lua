1184f8f585f098f184b4c17dba26ec55ddf6bf5a
Hayden Bevan <hayden.bevan@myob.com>
2025-12-10 23:53:42 +1100

    Use <leader>gf to search git files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,7 +15,7 @@ return {
             
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-            vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+            vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
             vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
             vim.keymap.set("n", "<leader>fs", builtin.treesitter, {})
