fe6a08b87ba0d617768f1503f0d684f33d6c9f8c
Hayden Bevan <hayden.bevan@myob.com>
2026-04-09 08:04:59 +1000

    Add keymap to open current file in GitHub

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,6 +3,10 @@ return {
         "tpope/vim-fugitive",
         config = function()
             vim.keymap.set("n", "<leader>gs", "<Cmd>tab Git<CR>")
+
+            vim.keymap.set("n", "<leader>gh", function()
+                vim.cmd("GBrowse")
+            end)
         end
     }
 }
