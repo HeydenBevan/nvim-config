d38f68f203fde0280fcdea1fda469e12fba5d8e8
Hayden Bevan <hayden.bevan@myob.com>
2026-06-12 07:21:49 +1000

    Use latest nvim-treesiter from main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,32 +1,42 @@
+local lst_utils = require("utils.lists")
+
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "lua",
-                    "bash",
-                    "toml",
-                    "yaml",
-                    "xml",
-                    "json",
-                    "html",
-                    "git_config"
-                },
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate',
+  branch = 'main',
+  config = function()
+      local ts_langs = {
+          "lua",
+          "bash",
+          "toml",
+          "yaml",
+          "xml",
+          "json",
+          "html",
+          "git_config",
+          "gitcommit",
+          "c_sharp",
+          "javascript"
+      }
 
-                indent = { enabled = true },
+      local ts = require("nvim-treesitter")
+      ts.install(ts_langs)
 
-                sync_install = false,
+      local installed_ts_langs = ts.get_installed()
+      local filetypes = {}
+      for _, tsl in pairs(installed_ts_langs) do
+          local fts = vim.treesitter.language.get_filetypes(tsl)
+          for _, l in pairs(fts) do
+              filetypes[#filetypes + 1] = l
+          end
+      end
 
-                auto_install = true,
-
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                }
-            })
-        end
-    }
+      vim.api.nvim_create_autocmd("FileType", {
+          pattern = filetypes,
+          callback = function(args)
+              vim.treesitter.start(args.buf)
+          end
+      })
+  end
 }
-
